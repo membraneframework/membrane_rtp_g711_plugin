b5e9383fea8cee0863b5e7d1614f9ca2d1b8098f
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2023-12-13 14:04:20 +0100

    Bump deps, release 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.G711.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/jellyfish-dev/membrane_rtp_g711_plugin"
 
   def project do
